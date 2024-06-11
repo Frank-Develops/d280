f7b7c8a5f0109a24a33b6414e3d258eebeedd9b4
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-06-11 02:50:10 -0500

    added info and css

--- d280_app/src/app/app.module.ts
+++ d280_app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MapComponent } from './map/map.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponent
+    MapComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
