ce74e9c4ab39c875cac1608fc4e0cf1480d8a2d7
Frank-Develops <167141334+Frank-Develops@users.noreply.github.com>
2024-06-11 20:49:51 -0500

    finished

--- d280_app/src/app/apicalls.service.ts
+++ d280_app/src/app/apicalls.service.ts
@@ -1,24 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-// import { Subject } from 'rxjs';
-// import { BehaviorSubject } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApicallsService {
-  // private data = new Subject<string>();
-  // data$ = this.data.asObservable();
-
+ 
   constructor(private http: HttpClient) {
 
    }
 
-  // countryData = new BehaviorSubject("Test");
 
   getCountryData(id): Observable<any> {
-    // console.log(`http://api.worldbank.org/v2/country/${id}?format=json`)
     return this.http.get(`http://api.worldbank.org/v2/country/${id}?format=json`);
   
   }
